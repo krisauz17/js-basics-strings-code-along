1291f3fb48d19be9d193d43dcb3a5ec7edb8f463
krishnendu basak <krisauz17@yahoo.com>
2022-03-21 12:05:28 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Krishnendu Basak';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const upperCaseCurrentUser = currentUser.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
